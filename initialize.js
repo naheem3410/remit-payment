2347dcfa6a91ebb1d67ad3b79578c77d33e0a2d1
naheem3410 <59153846+naheem3410@users.noreply.github.com>
2020-07-04 11:36:32 +0000

    Update initialize.js

--- initialize.js
+++ initialize.js
@@ -5,11 +5,17 @@ var https = require('https');
 var url = require('url');
 const PORT = process.env.PORT || 5000
 var post;
-var verifyPath;
+var verifyPath ='';
+var pendingPath='';
 var data='';
 var bankListPath;
+var jsonChargeData;
 var verifyAccountPath;
 var chargeData='';
+var jsonOTP;
+var jsonPin;
+var jsonBirthday;
+var jsonPhone;
 //make a server
 var server = http.createServer(function(req,res){
 	console.log(req.url);
@@ -81,8 +87,79 @@ var server = http.createServer(function(req,res){
   		});
 
 	}
+	else if(pathName.indexOf("/charge/") == 0){
+		console.log("EnteredPathVerify");
+		console.log(pathName);
+		pendingPath = pathName;
+		decide(req,res,pathName);
+
+	}
+	else if(pathName == "/charge/submit_otp"){
+		jsonOTP = '';
+		
+		console.log("OOTP"+pathName);
+		req.on('data', (chunk) => {
+    		jsonOTP += chunk;
+    		console.log(`BODY: ${data}`);
+  		});
+  		req.on('end', () => {
+    		console.log('No more data in request.');
+		console.log("CONSOLE ADD "+jsonOTP);
+		decide(req,res,pathName);
+    		//res.end(chargeData);
+   
+  		});
+	}
+	else if(pathName == "/charge/submit_pin"){
+		jsonPin = '';
+		
+		console.log("PIN"+pathName);
+		req.on('data', (chunk) => {
+    		jsonPin += chunk;
+    		console.log(`BODY: ${data}`);
+  		});
+  		req.on('end', () => {
+    		console.log('No more data in request.');
+		console.log("CONSOLE ADD "+jsonPin);
+		decide(req,res,pathName);
+    		//res.end(chargeData);
+   
+  		});
+	}
+	else if(pathName == "/charge/submit_birthday"){
+		jsonBirthday = '';
+		
+		console.log("BIRTHDAY"+pathName);
+		req.on('data', (chunk) => {
+    		jsonBirthday += chunk;
+    		console.log(`BODY: ${data}`);
+  		});
+  		req.on('end', () => {
+    		console.log('No more data in request.');
+		console.log("CONSOLE ADD "+jsonBirthday);
+		decide(req,res,pathName);
+    		//res.end(chargeData);
+   
+  		});
+	}
+	else if(pathName == "/charge/submit_phone"){
+		jsonPhone = '';
+		
+		console.log("PHONE"+pathName);
+		req.on('data', (chunk) => {
+    		jsonPhone += chunk;
+    		console.log(`BODY: ${data}`);
+  		});
+  		req.on('end', () => {
+    		console.log('No more data in request.');
+		console.log("CONSOLE ADD "+jsonPhone);
+		decide(req,res,pathName);
+    		//res.end(chargeData);
+   
+  		});
+	}
 	else{
-	res.end(JSON.stringify({"error":"MALFORMED URL"}));
+	res.end(JSON.stringify({"status":false,"message":"MALFORMED URL"}));
 	}	
 	}).listen(PORT);
 
@@ -93,7 +170,7 @@ function decide(req,res,pathName){
 	break;
 	case 'POST':decidePath(pathName,res);
 	break;
-	default:res.end(JSON.stringify({"error":"METHOD NOT SUPPORTED"}));
+	default:res.end(JSON.stringify({"status":false,"message":"METHOD UNSUPPORTED"}));
 	break;
 
 	}
@@ -106,20 +183,26 @@ function decidePath(pathName,res){
 	case '/initialize': initializeTransaction(res);
 	break;
 	case verifyPath: verifyTransaction(res,pathName);
-	break;	
+	break;
+	case pendingPath: checkPending(res,pathName);
+	break;
 	case '/bank': listBank(res,pathName);
 	break;
 	case '/bank/resolve': verifyAccount(res,pathName);
 	break;
 	case '/charge': chargeBankAccount(res,pathName);
 	break;
-	default:res.end(JSON.stringify({"error":"PATH NOT SUPPORTED"}));
+	case '/charge/submit_otp': submitOTP(res,pathName);
 	break;
-
-
-
+	case '/charge/submit_pin': submitPIN(res,pathName);
+	break;
+	case '/charge/submit_birthday': submitBirthday(res,pathName);
+	break;
+	case '/charge/submit_phone': submitPhone(res,pathName);
+	break;
+	default:res.end(JSON.stringify({"status":false,"message":"PATH UNSUPPORTED"}));
+	break;
 	}
-
 }
 //make request to Paystack to initialize transaction
 function initializeTransaction(response){
@@ -308,3 +391,203 @@ req.end();
 
 
 }
+//make request to Paystack to submit OTP
+function submitOTP(response,pathName){
+
+//options will be passed to the https instance
+	const options = {
+  method:"POST",
+  headers: {
+    'Authorization':'Bearer sk_test_3c01e91aad9edc6566860fabb83deade6385fadb',
+    'Content-Type':'application/json'
+  }
+};
+
+const req = https.request('https://api.paystack.co/charge/submit_otp',options, (res) => {
+  data = "";
+  
+  console.log('https://api.paystack.co/charge/submit_otp');
+  console.log(`STATUS: ${res.statusCode}`);
+  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+  //res.setEncoding('utf8');
+  res.on('data', (chunk) => {
+    data += chunk;
+    console.log(`BODY: ${data}`);
+  });
+  res.on('end', () => {
+    console.log('No more data in response.');
+    response.end(data);
+   
+  });
+});
+
+req.on('error', (e) => {
+  console.error(`problem with request: ${e.message}`);
+});
+
+// Write data to request body
+req.write(jsonOTP);
+req.end();
+
+
+
+}
+//make request to Paystack to submit PIN
+function submitPIN(response,pathName){
+
+//options will be passed to the https instance
+	const options = {
+  method:"POST",
+  headers: {
+    'Authorization':'Bearer sk_test_3c01e91aad9edc6566860fabb83deade6385fadb',
+    'Content-Type':'application/json'
+  }
+};
+
+const req = https.request('https://api.paystack.co/charge/submit_pin',options, (res) => {
+  data = "";
+  
+  console.log('https://api.paystack.co/charge/submit_pin');
+  console.log(`STATUS: ${res.statusCode}`);
+  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+  //res.setEncoding('utf8');
+  res.on('data', (chunk) => {
+    data += chunk;
+    console.log(`BODY: ${data}`);
+  });
+  res.on('end', () => {
+    console.log('No more data in response.');
+    response.end(data);
+   
+  });
+});
+
+req.on('error', (e) => {
+  console.error(`problem with request: ${e.message}`);
+});
+
+// Write data to request body
+req.write(jsonPin);
+req.end();
+
+
+
+}
+//make request to Paystack to submit birthday
+function submitBirthday(response,pathName){
+
+//options will be passed to the https instance
+	const options = {
+  method:"POST",
+  headers: {
+    'Authorization':'Bearer sk_test_3c01e91aad9edc6566860fabb83deade6385fadb',
+    'Content-Type':'application/json'
+  }
+};
+
+const req = https.request('https://api.paystack.co/charge/submit_birthday',options, (res) => {
+  data = "";
+  
+  console.log('https://api.paystack.co/charge/submit_birthday');
+  console.log(`STATUS: ${res.statusCode}`);
+  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+  //res.setEncoding('utf8');
+  res.on('data', (chunk) => {
+    data += chunk;
+    console.log(`BODY: ${data}`);
+  });
+  res.on('end', () => {
+    console.log('No more data in response.');
+    response.end(data);
+   
+  });
+});
+
+req.on('error', (e) => {
+  console.error(`problem with request: ${e.message}`);
+});
+
+// Write data to request body
+req.write(jsonBirthday);
+req.end();
+
+
+
+}
+//make request to Paystack to submit phone
+function submitPhone(response,pathName){
+//options will be passed to the https instance
+	const options = {
+  method:"POST",
+  headers: {
+    'Authorization':'Bearer sk_test_3c01e91aad9edc6566860fabb83deade6385fadb',
+    'Content-Type':'application/json'
+  }
+};
+
+const req = https.request('https://api.paystack.co/charge/submit_phone',options, (res) => {
+  data = "";
+  
+  console.log('https://api.paystack.co/charge/submit_phone');
+  console.log(`STATUS: ${res.statusCode}`);
+  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+  //res.setEncoding('utf8');
+  res.on('data', (chunk) => {
+    data += chunk;
+    console.log(`BODY: ${data}`);
+  });
+  res.on('end', () => {
+    console.log('No more data in response.');
+    response.end(data);
+   
+  });
+});
+
+req.on('error', (e) => {
+  console.error(`problem with request: ${e.message}`);
+});
+
+// Write data to request body
+req.write(jsonPhone);
+req.end();
+
+
+
+}
+//make request to Paystack to check pending
+function checkPending(response,pathName){
+
+//options will be passed to the https instance
+	const options = {
+  method:"GET",
+  headers: {
+    'Authorization':'Bearer sk_test_3c01e91aad9edc6566860fabb83deade6385fadb'
+  }
+};
+
+const req = https.request('https://api.paystack.co'+pendingPath,options, (res) => {
+  data = "";
+  
+  console.log('https://api.paystack.co'+pendingPath);
+  console.log(`STATUS: ${res.statusCode}`);
+  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+  //res.setEncoding('utf8');
+  res.on('data', (chunk) => {
+    data += chunk;
+    console.log(`BODY: ${data}`);
+  });
+  res.on('end', () => {
+    console.log('No more data in response.');
+    response.end(data);
+   
+  });
+});
+
+req.on('error', (e) => {
+  console.error(`problem with request: ${e.message}`);
+});
+
+req.end();
+
+}
+
